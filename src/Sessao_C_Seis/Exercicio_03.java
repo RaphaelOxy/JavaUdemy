93a6d3c07b7acbf9b3c83c2bb88c801ef8fd1ade
Raphael <raphael.siqueira4@gmail.com>
2024-10-26 13:14:11 -0300

    Exercicios 01-02 sessao 6

---
+++ src/Sessao_C_Seis/Exercicio_03.java
@@ -0,0 +1,13 @@
+package Sessao_C_Seis;
+import java.util.Scanner;
+
+public class Exercicio_03 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int tipo = sc.nextInt();
+
+        while(tipo != 4){
+            
+        }
+    }
+}
