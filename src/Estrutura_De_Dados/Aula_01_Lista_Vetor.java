256a19e0d38c7d524c210b35a6556e4ed69c81e7
Raphael <raphael.siqueira4@gmail.com>
2024-11-05 17:06:39 -0300

    Aula_01_EstruturaDeDados

---
+++ src/Estrutura_De_Dados/Aula_01_Lista_Vetor.java
@@ -0,0 +1,26 @@
+package Estrutura_De_Dados;
+
+public class Aula_01_Lista_Vetor {
+    public static void main(String[] args) {
+//        int x = 3;
+//        System.out.println("Valor de X: " + x);
+//        x = 4;
+//        System.out.println("Valor de X de novo: " + x);
+        int[] vetorzao = new int [10];
+        vetorzao[0] = 3;
+        vetorzao[1] = 4;
+        vetorzao[2] = 8;
+        vetorzao[3] = 13;
+        vetorzao[4] = 22;
+        vetorzao[5] = 51;
+        vetorzao[6] = 33;
+        
+
+//        System.out.println(vetorzao[3]);
+        System.out.println("Imprimindo...");
+        for (int i = 0; i < vetorzao.length ; i++) {
+            System.out.println(vetorzao[i]);
+        }
+
+    }
+}
